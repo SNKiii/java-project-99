20207bab9aba3701cc6d114edcc60cf2b436f099
SANky <ggsanky@gmail.com>
2026-06-17 01:48:19 +0300

    fix sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.sonarqube") version "6.0.1.5171"
+    id("org.sonarsource.scanner.gradle.sonar") version "7.3.1.8318"
 }
 
 group = "hexlet.code"
