e7da9f60ed2559a0fa9420caa1330b8c7e36252b
SANky <ggsanky@gmail.com>
2026-06-19 03:32:03 +0300

    add jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
-
 plugins {
     java
     id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.sonarqube") version "7.3.1.8318"
+    id("jacoco")
 }
 
 group = "hexlet.code"
@@ -64,12 +64,48 @@ tasks.test {
     jvmArgs("-XX:+EnableDynamicAgentLoading")
 }
 
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+        html.required = true
+    }
+    afterEvaluate {
+        classDirectories.setFrom(
+            files(classDirectories.files.map {
+                fileTree(it).apply {
+                    exclude(
+                        "**/hexlet/code/dto/**",
+                        "**/hexlet/code/model/**",
+                        "**/hexlet/code/config/**",
+                        "**/hexlet/code/exception/**",
+                        "**/hexlet/code/component/**",
+                        "**/hexlet/code/AppApplication.class"
+                    )
+                }
+            })
+        )
+    }
+}
+
 sonarqube {
     properties {
         property("sonar.projectKey", "SNKiii_java-project-99")
         property("sonar.organization", "snkiii")
         property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.coverage.jacoco.xmlReportPaths",
+            "${layout.buildDirectory.get()}/reports/jacoco/test/jacocoTestReport.xml"
+        )
+        property("sonar.coverage.exclusions",
+            "**/dto/**, **/model/**, **/config/**, **/exception/**, **/component/**, **/AppApplication.java"
+        )
+        property("sonar.tests", "src/test")
+        property("sonar.sources", "src/main")
     }
+}
+
+tasks.named("sonar") {
+    dependsOn("jacocoTestReport")
 }
 
 
